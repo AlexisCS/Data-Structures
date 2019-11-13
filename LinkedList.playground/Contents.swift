4084bfdd5b9bb48c7eb2dc8b2ee398724912b9ed
Alexis <alexis@hola.com>
2019-11-13 13:39:27 -0600

    Linked list completed

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -19,9 +19,68 @@ extension Node: CustomStringConvertible {
     }
 }
 
-let nodeOne = Node(value: "One")
-let nodeTwo = Node(value: "Two")
-let nodeThree = Node(value: "Three")
+struct LinkedList<T> {
+    var head: Node<T>?
+    var tail: Node<T>?
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    mutating func push(value: T) {
+        head = Node(value: value, nextNode: head)
+        if tail == nil {
+            tail = head
+        }
+    }
 
-nodeOne.nextNode = nodeTwo
-nodeTwo.nextNode = nodeThree
+    mutating func pop() -> T? {
+        defer {
+            head = head?.nextNode
+            if isEmpty {
+                tail = nil
+            }
+        }
+        return head?.value
+    }
+    
+    mutating func removeLast() -> T? {
+        guard let head = head else {
+            return nil
+        }
+        guard head.nextNode != nil else {
+            return pop()
+        }
+        var prev = head
+        var current = head
+        while let nextNode = current.nextNode {
+            prev = current
+            current = nextNode
+        }
+        prev.nextNode = nil
+        tail = prev
+        return current.value
+    }
+    
+    func insert(_ value: T, after node: Node<T>) {
+        node.nextNode = Node(value: value, nextNode: node.nextNode)
+    }
+    
+    func findNode(at index: Int) -> Node<T>? {
+        var currentIndex = 0
+        var currentNode = head
+        while (currentNode != nil && currentIndex < index) {
+            currentNode = currentNode?.nextNode
+            currentIndex += 1
+        }
+        return currentNode
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    var description: String {
+        guard let head = head else {
+            return "Empty list"
+        }
+        return String(describing: head)
+    }
+}
