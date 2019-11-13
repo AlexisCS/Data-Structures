f473851ea0c93d5c4fc56208a841e6a0d85f2308
Alexis <alexis@hola.com>
2019-11-13 12:03:27 -0600

    Add class node

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
-var str = "Hello, playground"
+class Node<T> {
+    var value: T
+    var nextNode: Node?
+    
+    init(value: T, nextNode: Node? = nil) {
+        self.value = value
+        self.nextNode = nextNode ?? nil
+    }
+}
+
+extension Node: CustomStringConvertible {
+    var description: String {
+        guard let node = nextNode else {
+            return "\(value)"
+        }
+        return "\(value) -> \(String(describing: node)) "
+    }
+}
+
+let nodeOne = Node(value: "One")
+let nodeTwo = Node(value: "Two")
+let nodeThree = Node(value: "Three")
+
+nodeOne.nextNode = nodeTwo
+nodeTwo.nextNode = nodeThree
